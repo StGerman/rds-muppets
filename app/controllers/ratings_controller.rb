5004ae9624a1cb87bf802edf5649f8a464ea533f
Dmitry Zuev <d.zuev@rambler-co.ru>
2016-12-10 19:01:08 +0300

    Add ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,11 +1,21 @@
 class RatingsController < ApplicationController
   def index
     respond_to do |format|
-      format.json do
-        render json: {}, status: :ok
-      end
+      format.json { render json: ratings.as_json, status: :ok }
     end
   end
 
-  def 
+  private
+
+  def ratings
+    User.connection.execute(%(
+      SELECT (
+        SELECT COUNT(*)
+        FROM emojis e
+        WHERE e.user_id = u.id AND created_at > CURRENT_DATE
+      ) AS emojis_today, u.id, u.nickname
+      FROM users u
+      ORDER BY emojis_today DESC
+    )).to_a
+  end
 end
