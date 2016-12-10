a10377c3149857e63912e8d7013e9729dff2b0af
Stanislav German <s.german@rambler-co.ru>
2016-12-10 16:55:42 +0300

    working on emoji

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    User.find(user_id)
+  end
+
+  def user_id
+    1
+  end
 end
--- app/models/emoji.rb
+++ app/models/emoji.rb
@@ -1,4 +1,8 @@
 class Emoji < ApplicationRecord
-  belongs_to :girl
-  belongs_to :user
+  belongs_to :girl, counter_cache: true
+  belongs_to :user, counter_cache: true
+
+  validates :presence, :kind
+
+  enum :kind, [:love, :hate, :crazy, :surprised]
 end
