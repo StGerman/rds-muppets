63866eda7e4ed3b2eb0ad04551fceef0dddc7563
Gordey Levchenko <g.levchenko@rambler-co.ru>
2016-12-10 14:49:18 +0300

    QRcode scanner init

---
+++ qrscan/qrtest/index.ios.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var React = require('react');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  NavigatorIOS,
+} = require('react-native');
+
+var QRCodeScreen = require('./QRCodeScreen');
+
+var qrtest = React.createClass({
+  render: function() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Index',
+          backButtonTitle: 'Back',
+          component: Index,
+        }}
+      />
+    );
+  }
+});
+
+var Index = React.createClass({
+
+  render: function() {
+    return (
+      <View style={styles.contentContainer}>
+        <TouchableOpacity onPress={this._onPressQRCode}>
+          <Text>Read QRCode</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  },
+
+  _onPressQRCode: function() {
+    this.props.navigator.push({
+      component: QRCodeScreen,
+      title: 'QRCode',
+      passProps: {
+        onSucess: this._onSucess,
+      },
+    });
+  },
+
+  _onSucess: function(result) {
+    console.log(result);
+  },
+
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
+
+AppRegistry.registerComponent('qrtest', () => qrtest);
